4203b10bc2c778c389b5f9306a6a31bd19fef379
leavesnight_ubuntu <zhuzhanghao9331@yahoo.co.jp>
2018-02-03 19:00:06 +0900

    Add encoder error term to the 2 frames motion-only BA for IMU+Encoder

--- Examples/RGB-D/RunRGBDVIEO.sh
+++ Examples/RGB-D/RunRGBDVIEO.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#OURFILE=~/test_save/test_001difficult2
-#OURFILE=~/dataset/test_003_corridor
-OURFILE=~/dataset/test_outdoor
+#OURFILE=~/dataset/Lab001easy
+#OURFILE=~/dataset/Lab002medium
+OURFILE=~/dataset/Corridor001easy
+#OURFILE=~/dataset/Corridor002medium
+#OURFILE=~/dataset/Corridor003difficult
+#OURFILE=~/dataset/Corridor004difficult
+#OURFILE=~/dataset/Farm002medium
 #EUROCFILE2=V203
 cd ~/zzh/ORB_SLAM2/Examples/RGB-D
 #./rgbd_tum ../../Vocabulary/ORBvoc.bin ./kinect2_sd.yaml $OURFILE $OURFILE/associate.txt $OURFILE/EncSensor.txt
-./rgbd_tum ../../Vocabulary/ORBvoc.bin ./kinect2_sd.yaml $OURFILE $OURFILE/associate.txt $OURFILE/odometrysensor.txt
+#./rgbd_tum ../../Vocabulary/ORBvoc.bin ./kinect2_sd.yaml $OURFILE $OURFILE/associate.txt $OURFILE/odometrysensor.txt
 CAMTYPE=RGBD
 
-#./rgbd_tum ../../Vocabulary/ORBvoc.bin ./kinect2_sd.yaml $OURFILE $OURFILE/associate.txt $OURFILE/IMUSensor.txt 9 0 $OURFILE/EncSensor.txt
+./rgbd_tum ../../Vocabulary/ORBvoc.bin ./kinect2_sd.yaml $OURFILE $OURFILE/associate.txt $OURFILE/IMUSensor.txt 9 0 $OURFILE/EncSensor.txt
 #./rgbd_tum ../../Vocabulary/ORBvoc.bin ./kinect2_sd.yaml $OURFILE $OURFILE/associate.txt $OURFILE/IMUSensor.txt 9
